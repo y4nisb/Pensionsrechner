ee083e315c733331b371f9b2991b1ea88f7904b5
yanisb <yanis.buelow@stud.kftg.ch>
2023-03-27 12:56:09 +0200

    Projekt aufgesetzt
    Projekt aufgesetzt und erstes Inputfeld hinzugefügt

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <label> Geben Sie ihr aktuelles Kapital an</label>
+      <input type="number" />
+    </div>
+  );
+}
+
+export default App;
