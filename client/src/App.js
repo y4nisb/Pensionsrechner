60e23726258c10a736654ff7d19b3111e4f419e3
yanisb <yanis.buelow@stud.kftg.ch>
2023-03-27 17:43:03 +0200

    input geld monatlich + css
    inputfeld hinzugefügt + ein wenig css

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <label> Geben Sie ihr aktuelles Kapital an</label>
-      <input type="number" />
+      <div className="input1">
+        <label> Geben Sie ihr aktuelles Kapital an</label>
+        <input type="number" />
+      </div>
+      <div className="input2">
+        <label> Wie viel möchten Sie monatlich sparen? </label>
+        <input type="number" />
+      </div>
     </div>
   );
 }
