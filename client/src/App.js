86ae200efb1ec20f25097ca1e4aed1584f62bf82
leonderGit <leon.gensch@stud.kftg.ch>
2023-03-28 15:52:45 +0200

    Task Board Item 4 Fertiggestellt
    Radio Buttons für Bezahlung am Anfang und Ende vom Monat hinzugefügt.

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,33 @@ function App() {
         <label> Geben Sie ihr aktuelles Kapital an</label>
         <input type="number" />
       </div>
-      <label>Monatlich</label>
-      <input type="radio" name="monatlich" value="monatlich" />
+
+      <br></br>
+
+      <div className="radio1">
+        <input type="radio" name="monatlich" value="monatlich" />
+        <label>Monatlich zahlen</label>
+      </div>
+
+      <br></br>
+
       <div className="input2">
         <label> Wie viel möchten Sie monatlich sparen? </label>
         <input type="number" />
+      </div>
+
+      <br></br>
+
+      <div className="radioMonatBezahlung">
+        <div className="radio2">
+          <input type="radio" name="bezAnfMonat" value="bezAnfMonat" />
+          <label>bez. Annfang Monat</label>
+        </div>
+
+        <div className="radio3">
+          <input type="radio" name="bezEndMonat" value="bezEndMonat" />
+          <label>bez. Ende Monat</label>
+        </div>
       </div>
     </div>
   );
