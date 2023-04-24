139f8434c41ad6b42d2107de6f59d1351d434f51
leonderGit <leon.gensch@stud.kftg.ch>
2023-04-24 18:23:01 +0200

    Endkapital und Zins eingeben & Monatlich Jährlich: Task 187 & 188: 1
    Ich habe das Fundament für die zweite Seite erstellt.

--- client/src/page3.js
+++ client/src/page3.js
@@ -2,6 +2,26 @@ function PAGE2() {
   return (
     <>
       <h1>Sparrechner</h1>
+      <div>
+        <div>
+          <label>Wie viel wollen Sie insgesamt sparen: </label>
+          <input type="text" />
+        </div>
+        <br></br>
+        <div>
+          <label>Wollen Sie monatlich oder jährlich sparen: </label>
+          <input type="radio" name="zinsRadio" value="monatlich" />
+          <label>Monatlich sparen</label>
+          <input type="radio" name="zinsRadio" value="jährlich" />
+          <label>Jährlich sparen</label>
+        </div>
+        <div>
+          <label>Wie viel sind sie bereit in dieser Zeitspanne zu sparen: </label>
+          <input type="text" />
+        </div>
+        <br></br>
+        <label>In ... Monaten und ... Jahren sind Sie fertig mit sparen.</label>
+      </div>
     </>
   );
 }
