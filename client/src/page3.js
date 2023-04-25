aabecfb40f98a52d95717317ef93ad8e6edc82ca
sami1968026 <sami.ferjani@stud.kftg.ch>
2023-04-25 17:25:30 +0200

    Elemente Einfügen Variablen deklarieren für Berechnen und einfügen  von Enddatum
    Enddatum eingefügt und mit Logik der Berechnung angefangen (variablen dafür deklariert und initialisiert)

--- client/src/page3.js
+++ client/src/page3.js
@@ -3,24 +3,44 @@ function PAGE2() {
     <>
       <h1>Sparrechner</h1>
       <div>
-        <div>
+        <div className="inputTextfield">
           <label>Wie viel wollen Sie insgesamt sparen: </label>
-          <input type="text" />
+          <input type="text" id="endkapital" />
         </div>
         <br></br>
         <div>
           <label>Wollen Sie monatlich oder jährlich sparen: </label>
-          <input type="radio" name="zinsRadio" value="monatlich" />
+          <input
+            type="radio"
+            name="zinsRadio"
+            value="monatlich"
+            id="monatlich"
+          />
           <label>Monatlich sparen</label>
-          <input type="radio" name="zinsRadio" value="jährlich" />
+          <input type="radio" name="zinsRadio" value="jährlich" id="jährlich" />
           <label>Jährlich sparen</label>
-        </div>
-        <div>
-          <label>Wie viel sind sie bereit in dieser Zeitspanne zu sparen: </label>
-          <input type="text" />
         </div>
         <br></br>
         <label>In ... Monaten und ... Jahren sind Sie fertig mit sparen.</label>
+      </div>
+      <br></br>
+      <div className="inputTextfield">
+        <label>Bis wann möchten Sie sparen: </label>
+        <input type="text" id="endpoint" />
+      </div>
+      <div className="berechnenButton">
+        <button
+          id="berechenenButton"
+          onClick={(event) => {
+            const jährlichZahlen = document.getElementById("jährlich");
+            const monatlichZahlen = document.getElementById("monatlich");
+            const result = document.getElementById("endkapital");
+            const now = Date;
+            const endpoint = document.getElementById("endpoint");
+          }}
+        >
+          Brechenen
+        </button>
       </div>
     </>
   );
