ecf11d4197f9da53443aa351c8932c359e016095
ルカ <118888153+Lucaherzog49@users.noreply.github.com>
2023-05-07 20:45:23 +0200

    Elemente einfügen : Logik berechnen Button : 3
    Berechungsweg überlegt => keinen wirklich umsetzbaren gefunden

--- client/src/page3.js
+++ client/src/page3.js
@@ -1,10 +1,11 @@
 function PAGE3() {
   let timecounter = 0; //die zeit bis wann gespart wird in monaten
+  let gesamtkeinzins; 
     return (
       <>
         <h1>Sparplaner</h1>
         <div>
-          <div className="inputTextfield">
+        <div className="inputTextfield">
             <label>Wie viel wollen Sie insgesamt sparen: </label>
             <input type="text" id="endkapital" />
           </div>
@@ -33,7 +34,7 @@ function PAGE3() {
         </div>
         <br></br>
         <div className="inputTextfield">
-          <label>Geben Sie den Zins an: </label>
+          <label>Geben Sie den Zins als Prozent an: </label>
           <input type="text" id="zins" />
         </div>
         <br></br>
@@ -45,21 +46,35 @@ function PAGE3() {
           <button
             id="berechenenButton"
             onClick={(event) => {
-              const jaehrlichZahlen = document.getElementById("jährlich");
+              //let monatlichZahlen = document.querySelector('input[name="zinsRadio"]')
+              const monatlichZahlen = document.getElementById("monatlich");
               const ziel = document.getElementById("endkapital");
               const now = new Date(); // creates a new Date object with the current date and time
               const year = now.getFullYear(); // gets the year (4 digits)
               const month = now.getMonth() + 1; // gets the month (0-11) and adds 1 to make it 1-12
+              const endMonth = document.getElementById("endMonth")
+              const endYear = document.getElementById("endYear")
               const zins = document.getElementById("zins");
               const endpoint = document.getElementById("endpoint");
   
-              console.log(now,year,month);
-              if (jaehrlichZahlen == true) {
-                endpoint.textContent =
-                  "In "+ timecounter +" Monaten sind Sie fertig mit sparen."
-              } else {
-                endpoint.textContent =
-                  "In "+ (timecounter / 12) +" Jahren sind Sie fertig mit sparen."
+
+              console.log(year,month,monatlichZahlen,ziel.value,now,year,endMonth.value,endYear.value,zins.value);
+              if (year < endYear && month < endMonth) {
+                endpoint.textContent="Das Datum ist in der Vergangenheit oder der gleiche Monat"
+                  
+              } else if(monatlichZahlen.checked){
+                const anzMonth = month - endMonth
+                for(x = 0; x < anzMonth; x++){
+                  gesamtkeinzins =+ anzMonth * zins
+                  console.log(monatseinzahlung)
+                  }
+                const gesamtmonat = gesamtkeinzins/anzMonth 
+                console.log(gesamtmonat)
+              }else{
+                const anzYear = year - endYear
+
+                timecounter = (anzYear / 12) + (month - endMonth)
+                endpoint.textContent=""
               }
             }}
           >
